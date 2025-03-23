76e4ca07e9ed467ee544736e32576a26a79fb0f3
machere13 <vladislav.chubarov13@yandex.ru>
2025-03-24 00:09:43 +0300

    feat: added super_short_article template

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -20,7 +20,8 @@ const htmlWebpackPlugins = [
   createPages('./src/pages/styleguide/styleguide.html', 'styleguide', ['styleguide']),
   createPages('./src/pages/form/form.html', 'form', ['form']),
   createPages('./src/pages/articles/big_articles/ba_article_01/ba_article_01.html', 'articles/letsplay', ['ba_article_01', 'O_header', 'O_footer']),
-  createPages('./src/pages/articles/short_articles/sa_article_01/sa_article_01.html', 'articles/colleguefriendship', ['ba_article_01', 'O_header', 'O_footer'])
+  createPages('./src/pages/articles/short_articles/sa_article_01/sa_article_01.html', 'articles/colleguefriendship', ['ba_article_01', 'O_header', 'O_footer']),
+  createPages('./src/pages/articles/super_short_articles/ssa_article_01.html', 'articles/deadlinecommunication', ['ba_article_01', 'O_header', 'O_footer'])
 ];
 
 module.exports = htmlWebpackPlugins;
