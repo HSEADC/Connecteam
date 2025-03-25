ea9bcbe0a9c7786a14cd64d38473bcf15864092f
machere13 <vladislav.chubarov13@yandex.ru>
2025-03-25 13:26:58 +0300

    feat: updated interactives page

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -13,7 +13,7 @@ const htmlWebpackPlugins = [
   createPages('./src/pages/articles/articles.html', 'articles', ['articles', 'header']),
   createPages('./src/pages/articles/short_article/short_article.html', 'articles/short_article', ['short_article', 'header', 'M_advice']),
   createPages('./src/pages/cases/cases.html', 'cases', ['cases', 'header']),
-  createPages('./src/pages/interactives/interactives.html', 'interactives', ['interactives', 'O_header']),
+  createPages('./src/pages/interactives/interactives.html', 'interactives', ['interactives', 'header']),
   createPages('./src/pages/cases/case/case.html', 'cases/case', ['case', 'header', 'sticker']),
   createPages('./src/pages/articles/article/article.html', 'articles/article', ['article', 'header', 'sticker']),
   createPages('./src/pages/interactives/interactive_start/interactive_start.html', 'interactives/interactive_start', ['interactive_start', 'header', 'sticker']),
