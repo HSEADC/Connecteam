2aaa573d81441361ea44b34a7a8a8881450497a9
machere13 <vladislav.chubarov13@yandex.ru>
2025-03-27 07:37:24 +0300

    feat: add links on article page

--- src/pages/articles/articles.js
+++ src/pages/articles/articles.js
@@ -81,10 +81,154 @@ document.addEventListener('DOMContentLoaded', function() {
         updateCardsVisibility();
     });
 
-    const feedbackCard = document.getElementById('I_article_feedback');
-    if (feedbackCard) {
-        feedbackCard.addEventListener('click', () => {
-            window.location.href = '/articles/feedback';
+    const feedbackArticle = document.getElementById('I_article_feedback');
+    const friendshipArticle = document.getElementById('I_article_friendship');
+    const deadlineCommunicationArticle = document.getElementById('I_article_deadlinecommunication');
+    const developersCoworkingArticle = document.getElementById('I_article_developerscoworking');
+    const letsPlayArticle = document.getElementById('I_article_letsplay');
+    const teamPsychologyMotivationArticle = document.getElementById('I_article_teampsychologymotivation');
+    const jobFriendshipArticle = document.getElementById('I_article_jobfriendship');
+    const newCommandMembersArticle = document.getElementById('I_article_newcommandmembers');
+    const problemTemplatesArticle = document.getElementById('I_article_problemtemplates');
+    const trustRoleArticle = document.getElementById('I_article_trustrole');
+    const teamMotivationArticle = document.getElementById('I_article_teammotivation');
+    const magicTabletArticle = document.getElementById('I_article_magictablet');
+    const diagnosticToolsArticle = document.getElementById('I_article_diagnostictools');
+    const roleTeamBuildingArticle = document.getElementById('I_article_roleteambuilding');
+    const qadeveloperscommunicationArticle = document.getElementById('I_article_qadeveloperscommunication');
+    const departmentscoldwarArticle = document.getElementById('I_article_departmentscoldwar');
+    const meetingtypesArticle = document.getElementById('I_article_meetingtypes');
+    const mediatorArticle = document.getElementById('I_article_mediator');
+    const techicaljargonArticle = document.getElementById('I_article_techicaljargon');
+    const waltercommunicationArticle = document.getElementById('I_article_waltercommunication');
+    const collegueargueArticle = document.getElementById('I_article_collegueargue');
+    const streamworkingArticle = document.getElementById('I_article_streamworking');
+    if (streamworkingArticle) {
+        streamworkingArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/streamworking';
+        });
+    }
+    if (collegueargueArticle) {
+        collegueargueArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/collegueargue';
+        });
+    }
+
+    if (waltercommunicationArticle) {
+        waltercommunicationArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/waltercommunication';
+        });
+    }
+
+    if (techicaljargonArticle) {
+        techicaljargonArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/techicaljargon';
+        });
+    }
+    if (mediatorArticle) {
+        mediatorArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/mediator';
+        });
+    }
+    if (meetingtypesArticle) {
+        meetingtypesArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/meetingtypes';
+        });
+    }
+
+    if (departmentscoldwarArticle) {
+        departmentscoldwarArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/departmentscoldwar';
+        });
+    }
+
+    if (qadeveloperscommunicationArticle) {
+        qadeveloperscommunicationArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/qadeveloperscommunication';
+        });
+    }
+
+    
+    if (roleTeamBuildingArticle) {
+        roleTeamBuildingArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/roleteambuilding';
+        });
+    }
+
+    
+    if (diagnosticToolsArticle) {
+        diagnosticToolsArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/diagnostictools';
+        });
+    }
+
+    if (magicTabletArticle) {
+        magicTabletArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/magictablet';
+        });
+    }
+
+    if (teamMotivationArticle) {
+        teamMotivationArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/teammotivation';
+        });
+    }
+
+    if (trustRoleArticle) {
+        trustRoleArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/trustrole';
+        });
+    }
+
+    if (problemTemplatesArticle) {
+        problemTemplatesArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/problemtemplates';
+        });
+    }
+
+    if (newCommandMembersArticle) {
+        newCommandMembersArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/newcommandmembers';
+        });
+    }
+
+    if (jobFriendshipArticle) {
+        jobFriendshipArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/jobfriendship';
+        });
+    }
+
+    if (teamPsychologyMotivationArticle) {
+        teamPsychologyMotivationArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/teampsychologymotivation';
+        });
+    }
+    if (letsPlayArticle) {
+        letsPlayArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/letsplay';
+        });
+    }
+    if (developersCoworkingArticle) {
+        developersCoworkingArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/developerscoworking';
+        });
+    }
+
+    if (deadlineCommunicationArticle) {
+        deadlineCommunicationArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/deadlinecommunication';
+        });
+    }
+
+    if (friendshipArticle) {
+        friendshipArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/friendship';
+        });
+    }
+
+    if (feedbackArticle) {
+        feedbackArticle.addEventListener('click', () => {
+            window.location.href = '/Connecteam/articles/feedback';
         });
     }
 
